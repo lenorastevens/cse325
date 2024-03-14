bb682041e4a39174f781283d14dc6865898029fa
Lenora Stevens <114180320+lenorastevens@users.noreply.github.com>
2024-03-14 15:53:41 -0500

    Beginning of SWA University project

---
+++ SWAUniversity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SWAUniversity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SWAUniversity/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SWAUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        [Required, StringLength(50, MinimumLength = 2), Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required, StringLength(50), Display(Name = "First Name")]
+        public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true), Display(Name = "Enrollement Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+        public string FullName 
+        {
+            get { return LastName + ", " + FirstMidName; }
+        }
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
